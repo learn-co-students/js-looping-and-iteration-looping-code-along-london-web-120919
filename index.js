d22cb4b68ce185fb173ae7bbacffee96c631b574
RogeredBacon <github email address>
2020-01-27 13:09:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+let array = [];
+function writeCards(names) {
+	for (let index = 0; index < names.length; index++) {
+		array.push(`Thank you, ${names[index]}, for the wonderful surprise gift!`);
+	}
+	return array;
+}
+
+function countDown(number) {
+	for (let index = number; index >= 0; index--) {
+		console.log(index);
+	}
+}
