8603c6408659eb2a2699c38cd9e40563be9ae734
Keemo Watson <keemowatson1@gmail.com>
2020-01-27 19:30:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(array, occasion){
+  let speech = []
+  for(let i = 0; i < array.length; i++)
+  speech.push(`Thank you, ${array[i]}, for the wonderful ${occasion} gift!`)
+  return speech
+};
+
+function countDown(number){
+  while(number > -1){
+    console.log(number--)
+  }
+}
