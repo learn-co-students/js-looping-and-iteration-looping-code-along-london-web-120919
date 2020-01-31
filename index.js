edbd1beccf03af14a3eb83ce52427d40e462b045
Tommy Berman <tomy.berman1@gmail.com>
2020-01-31 13:24:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(arrayName, eventName) {
+    const cardarr = []
+    for (let i = 0; i < arrayName.length; i++ ) {
+        cardarr.push(`Thank you, ${arrayName[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return cardarr;
+}
+
+function countDown(posInt) {
+    let i = posInt
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
